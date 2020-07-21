40e78a4cc7403f009095244bcfa657f485df068b
Jay Patwardhan <jaypatwardhan@ucsb.edu>
2020-07-21 15:54:16 -0700

    Add files via upload

---
+++ ResNet/ResNet.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as  nn
+
+
+"""
+ResNet 50, 101, 152 implementations in PyTorch
+
+References;
+1. Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+   Deep Residual Learning for Image Recognition 
+   https://arxiv.org/pdf/1512.03385.pdf
+
+2. https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+
+3. https://github.com/AladdinPerzon/Machine-Learning-Collection/blob/master/ML/Pytorch/CNN_architectures/pytorch_resnet.py
+"""
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, i_downsample=None, stride=1):
+        super(ResBlock, self).__init__()
+        self.expansion = 4
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.batch_norm1 = nn.BatchNorm2d(out_channels)
+        
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.batch_norm2 = nn.BatchNorm2d(out_channels)
+        
+        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, stride=1, padding=0)
+        self.batch_norm3 = nn.BatchNorm2d(out_channels*self.expansion)
+        
+        self.i_downsample = i_downsample
+        self.stride = stride
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        identity = x.clone()
+        x = self.conv1(x)
+        x = self.batch_norm1(x)
+        x = self.relu(x)
+        
+        x = self.conv2(x)
+        x = self.batch_norm2(x)
+        x = self.relu(x)
+        
+        x = self.conv3(x)
+        x = self.batch_norm3(x)
+        
+        #downsample if needed
+        if self.i_downsample is not None:
+            identity = self.i_downsample(identity)
+        #add identity
+        x+=identity
+        x=self.relu(x)
+        
+        return x
+        
+        
+class ResNet(nn.Module):
+    def __init__(self, ResBlock, layer_list, num_classes, num_channels=3):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        
+        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.batch_norm1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.max_pool = nn.MaxPool2d(kernel_size = 3, stride=2, padding=1)
+        
+        self.layer1 = self._make_layer(ResBlock, layer_list[0], planes=64)
+        self.layer2 = self._make_layer(ResBlock, layer_list[1], planes=128, stride=2)
+        self.layer3 = self._make_layer(ResBlock, layer_list[2], planes=256, stride=2)
+        self.layer4 = self._make_layer(ResBlock, layer_list[3], planes=512, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512*4, num_classes)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.batch_norm1(x)
+        x = self.relu(x)
+        x = self.max_pool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+        
+        return x
+        
+    def _make_layer(self, ResBlock, blocks, planes, stride=1):
+        i_downsample = None
+        layers = []
+        
+        if stride != 1 or self.in_channels != planes*4:
+            i_downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, planes*4, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes*4)
+            )
+            
+        layers.append(ResBlock(self.in_channels, planes, i_downsample, stride))
+        self.in_channels = planes*4
+        
+        for i in range(blocks-1):
+            layers.append(ResBlock(self.in_channels, planes))
+            
+        return nn.Sequential(*layers)
+        
+        
+def ResNet50(num_classes, channels=3):
+    return ResNet(ResBlock, [3,4,6,3], num_classes, channels)
+    
+def ResNet101(num_classes, channels=3):
+    return ResNet(ResBlock, [3,4,23,3], num_classes, channels)
+
+def ResNet152(num_classes, channels=3):
+    return ResNet(ResBlock, [3,8,36,3], num_classes, channels)
+
